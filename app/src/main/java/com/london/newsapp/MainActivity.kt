4d57f041096ec5db98dbe7eeb9182baf004cb015
theMIslam <islam.medetbekov.01@gmail.com>
2023-11-09 15:35:06 +0600

    First commit

---
+++ app/src/main/java/com/london/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.london.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.london.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
