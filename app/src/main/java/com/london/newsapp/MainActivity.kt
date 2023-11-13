145d524343bf1d88c836bc4f3a46b7b12772c08c
theMIslam <islam.medetbekov.01@gmail.com>
2023-11-13 16:01:51 +0600

    added system ui controller

--- app/src/main/java/com/london/newsapp/MainActivity.kt
+++ app/src/main/java/com/london/newsapp/MainActivity.kt
@@ -2,18 +2,23 @@ package com.london.newsapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowInsets.Side
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.london.newsapp.domain.usecases.AppEntryUseCase
 import com.london.newsapp.domain.usecases.SaveAppEntryUseCase
 import com.london.newsapp.presentation.nvgraph.NavGraph
@@ -47,9 +52,18 @@ class MainActivity : ComponentActivity() {
 //            }
 //        }
         setContent {
-            NewsAppTheme(
-                dynamicColor = false
-            ) {
+            NewsAppTheme {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = ! isSystemInDarkMode
+                    )
+                }
+
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
                     val startDestination = viewModel.startDestination
                     NavGraph(startDestination = startDestination)
--- app/src/main/java/com/london/newsapp/MainViewModel.kt
+++ app/src/main/java/com/london/newsapp/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.london.newsapp
 
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
